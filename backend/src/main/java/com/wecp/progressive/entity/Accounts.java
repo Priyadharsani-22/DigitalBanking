c0a73704ec225ba085f4cd70fec571f675705222
username <wecp-dummy@gmail.com>
2024-07-19 06:01:07 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/entity/Accounts.java
+++ backend/src/main/java/com/wecp/progressive/entity/Accounts.java
@@ -1,5 +1,202 @@
+// // package com.wecp.progressive.entity;
+
+// // // import javax.persistence.Entity;
+// // // import javax.persistence.GeneratedValue;
+// // // import javax.persistence.GenerationType;
+// // // import javax.persistence.Id;
+// // // import javax.persistence.ManyToOne;
+
+// // //@Entity
+// // public class Accounts {
+
+// //     //@Id
+// //     //@GeneratedValue(strategy = GenerationType.IDENTITY)
+// //     private int accountId;
+// //     private int customerId;
+// //     private double balance;
+
+// //     //@ManyToOne
+// //     //private Customers customers;
+
+// //     public Accounts(){
+
+// //     }
+    
+
+// //     public Accounts(int accountId, int customerId, double balance) {
+// //         this.accountId = accountId;
+// //         this.customerId = customerId;
+// //         this.balance = balance;
+// //     }
+
+
+// //     public int getAccountId() {
+// //         return accountId;
+// //     }
+
+// //     public void setAccountId(int accountId) {
+// //         this.accountId = accountId;
+// //     }
+
+// //     public int getCustomerId() {
+// //         return customerId;
+// //     }
+
+// //     public void setCustomerId(int customerId) {
+// //         this.customerId = customerId;
+// //     }
+
+// //     public double getBalance() {
+// //         return balance;
+// //     }
+
+// //     public void setBalance(double balance) {
+// //         this.balance = balance;
+// //     }
+    
+    
+
+// // //     public Accounts(Integer accountId, Integer customerId, Double balance) {
+// // //         this.accountId = accountId;
+// // //         this.customerId = customerId;
+// // //         this.balance = balance;
+// // //     }
+
+
+// // //     public Integer getAccountId() {
+// // //         return accountId;
+// // //     }
+
+// // //     public void setAccountId(Integer accountId) {
+// // //         this.accountId = accountId;
+// // //     }
+
+// // //     public Integer getCustomerId() {
+// // //         return customerId;
+// // //     }
+
+// // //     public void setCustomerId(Integer customerId) {
+// // //         this.customerId = customerId;
+// // //     }
+
+// // //     public Double getBalance() {
+// // //         return balance;
+// // //     }
+
+// // //     public void setBalance(Double balance) {
+// // //         this.balance = balance;
+// // //     }
+
+// // //     // public Customers getCustomers() {
+// // //     //     return customers;
+// // //     // }
+
+// // //     // public void setCustomers(Customers customers) {
+// // //     //     this.customers = customers;
+// // //     // }
+
+    
+
+
+    
+
+// //  }
+// package com.wecp.progressive.entity;
+
+// public class Accounts implements Comparable<Accounts> {
+
+//     private int accountId;
+//     private int customerId;
+//     private double balance;
+
+//     public Accounts() {
+//         // constrcutor
+//     }
+
+//     public Accounts(int accountId, int customerId, double balance) {
+//         this.accountId = accountId;
+//         this.customerId = customerId;
+//         this.balance = balance;
+//     }
+
+//     // Getters and setters
+//     public int getAccountId() {
+//         return accountId;
+//     }
+
+//     public void setAccountId(int accountId) {
+//         this.accountId = accountId;
+//     }
+
+//     public int getCustomerId() {
+//         return customerId;
+//     }
+
+//     public void setCustomerId(int customerId) {
+//         this.customerId = customerId;
+//     }
+
+//     public double getBalance() {
+//         return balance;
+//     }
+
+//     public void setBalance(double balance) {
+//         this.balance = balance;
+//     }
+
+//     @Override
+//     public int compareTo(Accounts otherAccounts) {
+//         // Implement comparison logic based on account balance
+//         return Double.compare(this.getBalance(), otherAccounts.getBalance());
+//     }
+// }
+
 package com.wecp.progressive.entity;
 
-public class Accounts {
+public class Accounts implements Comparable<Accounts> {
 
+    private int accountId;
+    private int customerId;
+    private double balance;
+
+    public Accounts() {
+        // constrcutor
+    }
+
+    public Accounts(int accountId, int customerId, double balance) {
+        this.accountId = accountId;
+        this.customerId = customerId;
+        this.balance = balance;
+    }
+
+    // Getters and setters
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public int compareTo(Accounts otherAccounts) {
+        // Implement comparison logic based on account balance
+        return Double.compare(this.getBalance(), otherAccounts.getBalance());
+    }
 }
