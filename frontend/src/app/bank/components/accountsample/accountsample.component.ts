5172fe101760908647c67d7fd7534215741ea3b4
username <wecp-dummy@gmail.com>
2024-07-24 13:04:33 +0000

    Day16andDay17 Completed

--- frontend/src/app/bank/components/accountsample/accountsample.component.ts
+++ frontend/src/app/bank/components/accountsample/accountsample.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-accountsample',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './accountsample.component.html',
+//   styleUrls: ['./accountsample.component.css']
+// })
+// export class AccountsampleComponent {
+  
+// }
+import { Component } from '@angular/core';
+import { AccountTS } from '../../types/tstypes/Accountts';
+ 
 @Component({
   selector: 'app-accountsample',
   standalone: true,
   imports: [],
   templateUrl: './accountsample.component.html',
-  styleUrl: './accountsample.component.css'
+  styleUrls:[ './accountsample.component.css']
 })
 export class AccountsampleComponent {
-
+  account: AccountTS;
+ 
+  constructor(){
+    this.account = new AccountTS("1", 1000.00, "1");
+  }
+ 
 }
--- frontend/src/app/bank/components/customersample/customersample.component.ts
+++ frontend/src/app/bank/components/customersample/customersample.component.ts
@@ -1,16 +1,39 @@
+// import { Component } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { CustomerTS } from '../../types/tstypes/Customerts';
+// import { of } from 'rxjs';
+
+
+// @Component({
+//   selector: 'app-customersample',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './customersample.component.html',
+//   styleUrls: ['./customersample.component.css']
+// })
+// export class CustomersampleComponent {
+ 
+// }
+
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Customer } from '../../types/Customer';
+import { CustomerTS } from '../../types/tstypes/Customerts';
 import { of } from 'rxjs';
-
+ 
 @Component({
   selector: 'app-customersample',
   standalone: true,
   imports: [],
   templateUrl: './customersample.component.html',
-  styleUrl: './customersample.component.css'
+  styleUrls: ['./customersample.component.css']
 })
 export class CustomersampleComponent {
  
+  customer : CustomerTS;
+ 
+  constructor(){
+    this.customer= new CustomerTS( "John Doe", "john@example.com", "john_doe", "password123", "User","1");
+   
+  }
 }
 
